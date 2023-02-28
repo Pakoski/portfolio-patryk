5ec05da9a0201ae7a581fe59517737da0bc2c389
Patryk <patrykntrzba@gmail.com>
2023-02-28 23:27:05 +0100

    1 version du front de mon portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,116 @@
+"use strict";
+
+function createParticles(elementId) {
+  const canvas = document.getElementById(elementId);
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  const ctx = canvas.getContext("2d");
+
+  const particles = [];
+  const particleCount = 10;
+  const shapes = ["triangle"];
+
+  // Generate particle objects
+  for (let i = 0; i < particleCount; i++) {
+    const particle = {
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      size: Math.random() * 60 + 25,
+      shape: shapes[Math.floor(Math.random() * shapes.length)],
+      speedX: (Math.random() - 0.5) * 2,
+      speedY: (Math.random() - 0.5) * 2,
+      angle: 0,
+      angleSpeed: (Math.random() - 0.5) * 0.005,
+    };
+    particles.push(particle);
+  }
+
+  let isDrawing = false;
+
+  function draw() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < particles.length; i++) {
+      const particle = particles[i];
+      ctx.beginPath();
+    
+       if (particle.shape === "triangle") {
+        ctx.save();
+        ctx.translate(particle.x, particle.y);
+        ctx.rotate(particle.angleSpeed);
+        ctx.rotate(particle.angle);
+        ctx.rotate(-particle.angleSpeed);
+        ctx.translate(-particle.x, -particle.y);
+        ctx.moveTo(particle.x, particle.y - particle.size);
+        ctx.lineTo(particle.x + particle.size, particle.y + particle.size);
+        ctx.lineTo(particle.x - particle.size, particle.y + particle.size);
+        ctx.closePath();
+
+        ctx.restore();
+      }
+    
+      ctx.fillStyle = "rgba(32, 25, 62, 0.1)";
+      ctx.fill();
+    
+      particle.x += particle.speedX;
+      particle.y += particle.speedY;
+      particle.angle += particle.angleSpeed;
+    
+      // Check if particle is out of bounds
+      if (particle.x > canvas.width || particle.x < 0) {
+        particle.speedX = -particle.speedX;
+        particle.angleSpeed = (Math.random()) * 0.008;
+      }
+      if (particle.y > canvas.height || particle.y < 0) {
+        particle.speedY = -particle.speedY;
+        particle.angleSpeed = -(Math.random()) * 0.008;
+      }
+      ctx.restore(); // restore the previous context state
+    }
+
+        if (isDrawing) {
+      requestAnimationFrame(draw);
+    }
+  }
+
+  return {
+    start: function() {
+      isDrawing = true;
+      requestAnimationFrame(draw);
+    },
+    stop: function() {
+      isDrawing = false;
+    },
+  };
+}
+
+const canvas1 = createParticles("canvas1");
+const canvas2 = createParticles("canvas2");
+const canvas3 = createParticles("canvas3");
+const canvas4 = createParticles("canvas4");
+
+canvas1.start();
+canvas2.start();
+canvas3.start();
+canvas4.start();
+
+//menu deroulant mobile
+
+let content = document.querySelector("#hamburger-content"),
+    sidebarBody = document.querySelector("#hamburger-sidebar-body"),
+    btn = document.querySelector("#hamburger-button"),
+    overlay = document.querySelector("#hamburger-overlay"),
+    activatedClass = "hamburger-activated";
+
+sidebarBody.innerHTML = content.innerHTML;
+
+btn.addEventListener("click", function () {
+    this.parentNode.classList.add(activatedClass);
+});
+
+overlay.addEventListener("click", function () {
+    this.parentNode.classList.remove(activatedClass);
+});
+
---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.min.css">
+    <title>Portfolio - Patryk Nietrzeba - Développeur Web Junior</title>
+    <meta name="description" content="Portfolio Web de Patryk Nietrzeba Développeur Web Junior">
+</head>
+<body>
+<header>
+    <div id="hamburger">
+        <a class="logo" href="#folio_home">
+            <img  src="assets/images/logoVb.svg">
+        </a>
+        <div id="hamburger-content">
+            <nav>
+                <ul>
+                    <li><a href="#folio_home">Home</a></li>
+                    <li><a href="#folio_propos">à Propos</a></li>
+                    <li><a href="#folio_projet">Projets</a></li>
+                    <li><a href="#folio_contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+        <button id="hamburger-button">&#9776;</button>
+        <div id="hamburger-sidebar">
+            <div id="hamburger-sidebar-header">
+                <img class="logMenu" src="assets/images/logo.svg">
+            </div>
+            <div id="hamburger-sidebar-body"></div>
+        </div>
+        <div id="hamburger-overlay"></div>
+    </div> 
+</header>
+
+<section id="folio_home" class="section-canvas">
+    <div class="divHome">
+        <div class="texteHome">
+            <h1>Bonjour ! </h1>
+            <h2>Je suis un Développeur Web Junior</h2>
+        </div>
+        <div class="containerTriangle">
+            <div class="zoneTriangle">
+                <div class="triangles bas"></div>
+                <div class="triangles haut"></div>
+                <div class="triangles gauche"></div>
+                <div class="triangles droite"></div>
+                <div class="triangles upLvBas"></div>
+                <div class="triangles haut upLvD"></div>
+                <div class="triangles upLvLastd"></div>
+                <div class="triangles haut upLvG"></div>
+                <div class="triangles upLvLastG"></div>
+                <div class="triangles2"></div>
+                <div class="triangles2Bas"></div>
+                <div class="triangles2 droit2"></div>
+                <div class="trianglesElar"></div>
+                <div class="trianglesElar mirror"></div>
+                <div class="trianglesElar2"></div>
+                <div class="trianglesElar2 mirror2"></div>
+                <div class="trianglesElar3"></div>
+                <div class="trianglesElar3 mirror3"></div>
+            </div>
+        </div>
+    </div>
+    <canvas id="canvas1"></canvas>
+</section>
+
+<section id="folio_propos" class="section-canvas">
+    <div class="containerAbout">
+        <div class="about">
+            <div class="imgAbout">
+                <img src="assets/images/ja.png" alt="portrait sur illustrator">
+            </div>
+            <div class="texteMoi">
+                <h2>Bienvenue !</h2>
+                <h3>Je m'appelle Patryk</h3>
+            </div>
+            <div class="clearFloat"></div>
+            <div class="textePropos">
+                <p>J'ai obtenu une licence LLCER Polonais avant de me lancer dans le développement web, et j'ai depuis acquis des compétences solides en programmation, en design et en gestion de projet. Mon objectif est de devenir un développeur web full stack polyvalent.</p>
+                <br>
+                <p>Je suis un passionné d'informatique depuis mon plus jeune âge et j'ai toujours été fasciné par les jeux vidéo, en particulier les MMORPG. Je crois que ma passion pour les jeux vidéo m'a aidé à développer ma créativité et mon esprit d'équipe, ce qui se reflète dans mon approche de la programmation et du développement web.</p>
+                <br>
+                <p>Si vous recherchez un développeur web passionné et créatif je suis toujours ouvert aux nouvelles opportunités de collaboration.</p>
+            </div>
+        </div>
+    </div> 
+    <h2 class="skills">Mes Compétences</h2>
+    <div class="articlesSkills">
+        <article></article>
+        <article></article>
+    </div>
+    <canvas id="canvas2"></canvas>
+</section>
+
+<section id="folio_projet" class="section-canvas">
+    <div class="containerProjet">
+        <div class="tri">
+            <div class="overlay">
+                <a href="#folio_home"></a>
+                <h3>PROJET 1</h3>
+            </div>
+            <img src="assets/images/ja.png">  
+        </div>
+        <div class="tri2">
+            <div class="overlay">
+                <a href="#folio_home"></a>
+                <h3>PROJET 1</h3>
+            </div>
+            <img src="assets/images/ja.png">
+        </div>
+        <div class="tri">
+            <div class="overlay">
+                <a href="#folio_home"></a>
+                <h3>PROJET 1</h3>
+            </div>
+            <img src="assets/images/ja.png">
+        </div>
+        <div class="tri2">
+            <div class="overlay">
+                <a href="#folio_propos"></a>
+                <h3>PROJET 1</h3>
+            </div>
+            <img src="assets/images/ja.png">
+        </div>
+        <div class="tri">
+            <div class="overlay">
+                <a href="#folio_contact"></a>
+                <h3>PROJET 1</h3>
+            </div>
+            <img src="assets/images/ja.png">
+        </div>
+    </div>  
+    <canvas id="canvas3"></canvas>
+</section>
+
+<section id="folio_contact" class="section-canvas">
+    <h3>Contactez-moi</h3>
+    <p>Si vous voulez m'envoyer n'hésite pas a me faire passer un message grâce au formulaire ci dessous.</p>
+    <div class="containerContact">
+        <form method="post" enctype="multipart/form-data">
+            <div>
+                <label for="nom">Nom</label>
+                <input type="text" name="nom" id="nom" required value="">
+            </div>
+            <div>
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email">
+            </div>
+            <div>
+                <label for="texte">Texte</label>
+                <textarea name="texte" id="texte"></textarea>
+            </div>
+
+            <div>
+                <button type="submit">Enregistrer</button>
+                <button type="reset">Effacer</button>
+            </div>
+        </form>
+    </div>
+
+    <canvas id="canvas4"></canvas>
+</section>
+
+<footer>
+    
+</footer>
+<script src="assets/js/script.js"></script>
+
+</body>
+</html>
